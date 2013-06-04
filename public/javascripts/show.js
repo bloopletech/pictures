169ff7c6410dcce00db9f393d38fb77af3f9dcb8
Brenton "B-Train" Fletcher <i@bloople.net>
2013-06-04 23:44:51 +1000

    Fix image zooming

--- public/javascripts/show.js
+++ public/javascripts/show.js
@@ -10,19 +10,27 @@ $(function() {
   });
 
   function showPerformer() {
-    performer.show().css({ marginLeft: -(performer.width() / 2) + "px", marginTop: -(performer.height() / 2) + "px" });
+    performer.show().css({ marginLeft: -(performer.width() / 2) + "px", marginTop: -(performer.height() / 2) + "px", "left": "50%", "top": "50%" });
   }
 
   performer.load(showPerformer).click(function(event) {
-    if(performer.css("maxWidth") == "100%") performer.css({ maxWidth: "", maxHeight: "" });
-    else performer.css({ maxWidth: "100%", maxHeight: "100%" });
-    showPerformer();
+    if(performer.css("maxWidth") == "100%") {
+      performer.css({ maxWidth: "", maxHeight: "" });
+      showPerformer();
+
+      if(performer.width() > backdrop.width()) performer.css({ "left": "0", "marginLeft": "0" });
+      if(performer.height() > backdrop.height()) performer.css({ "top": "0", "marginTop": "0" });
+    }
+    else {
+      performer.css({ maxWidth: "100%", maxHeight: "100%" });
+      showPerformer();
+    }
 
     event.stopPropagation();
   });
 
   $("li.picture > .wrapper > a").live("click", function(event) {
-    performer.css({ maxWidth: "100%", maxHeight: "100%", marginLeft: "0", marginTop: "0"}).hide();
+    performer.css({ maxWidth: "100%", maxHeight: "100%", marginLeft: "0", marginTop: "0" }).hide();
     performer.attr("src", $(this).attr("href"));
     backdrop.show();
 
